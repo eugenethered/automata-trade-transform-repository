22e96da4f5e46d96ba8a7833e34430d0a81c4b97
Eugene the Red <me@eugenethered.com>
2022-06-22 15:07:23 +0100

    employing hash

--- tests/repository/TradeTransformRepository_test.py
+++ tests/repository/TradeTransformRepository_test.py
@@ -1,6 +1,7 @@
 import unittest
 
 from cache.holder.RedisCacheHolder import RedisCacheHolder
+from cache.provider.RedisCacheProviderWithHash import RedisCacheProviderWithHash
 
 from tradetransformrepo.TradeTransform import TradeTransform
 from tradetransformrepo.repository.TradeTransformRepository import TradeTransformRepository
@@ -12,19 +13,19 @@ class TradeTransformRepositoryTestCase(unittest.TestCase):
         options = {
             'REDIS_SERVER_ADDRESS': '192.168.1.90',
             'REDIS_SERVER_PORT': 6379,
-            'TRADE_TRANSFORMATIONS_KEY': 'test:transformation:trade'
+            'TRADE_TRANSFORMATIONS_KEY': 'test:mv:transformation:trade'
         }
-        self.cache = RedisCacheHolder(options)
+        self.cache = RedisCacheHolder(options, held_type=RedisCacheProviderWithHash)
         self.repository = TradeTransformRepository(options)
 
     def tearDown(self):
-        self.cache.delete('test:transformation:trade')
+        self.cache.delete('test:mv:transformation:trade')
 
     def test_should_store_and_retrieve_trade_transform(self):
         trade_transform = TradeTransform('BTC/OTC', {
             'instrument': 'BTCOTC'
         })
-        self.repository.append(trade_transform)
+        self.repository.create(trade_transform)
         stored_trade_transformations = self.repository.retrieve()
         self.assertEqual(trade_transform, stored_trade_transformations[0])
 
@@ -37,7 +38,7 @@ class TradeTransformRepositoryTestCase(unittest.TestCase):
         })
         trade_transform_3 = TradeTransform('GBP/OTC')
         trade_transformations = [trade_transform_1, trade_transform_2, trade_transform_3]
-        self.repository.store(trade_transformations)
+        self.repository.store_all(trade_transformations)
         stored_trade_transformations = self.repository.retrieve()
         self.assertEqual(trade_transformations, stored_trade_transformations)
 
--- tradetransformrepo/repository/TradeTransformRepository.py
+++ tradetransformrepo/repository/TradeTransformRepository.py
@@ -2,6 +2,7 @@ import logging
 from typing import List
 
 from cache.holder.RedisCacheHolder import RedisCacheHolder
+from cache.provider.RedisCacheProviderWithHash import RedisCacheProviderWithHash
 from core.options.exception.MissingOptionError import MissingOptionError
 
 from tradetransformrepo.TradeTransform import TradeTransform
@@ -18,7 +19,7 @@ class TradeTransformRepository:
         self.log.info('initializing')
         self.options = options
         self.__check_options()
-        self.cache = RedisCacheHolder()
+        self.cache = RedisCacheHolder(held_type=RedisCacheProviderWithHash)
 
     def __check_options(self):
         if self.options is None:
@@ -28,38 +29,32 @@ class TradeTransformRepository:
             self.log.warning(f'missing option please provide option {TRADE_TRANSFORMATIONS_KEY}')
             raise MissingOptionError(f'missing option please provide option {TRADE_TRANSFORMATIONS_KEY}')
 
-    def append(self, transform):
-        self.log.debug(f'appending trade transform:[{transform}]')
-        self.__store_overwrite(transform)
+    def store_key(self):
+        return self.options[TRADE_TRANSFORMATIONS_KEY]
 
-    def store(self, transformations):
+    @staticmethod
+    def value_key(transform):
+        return f'{transform["trade"]}'
+
+    def create(self, transform):
+        self.update(transform)
+
+    def update(self, transform):
+        self.log.debug(f'setting trade transform:[{transform}]')
+        serialized_entity = serialize_trade_transform(transform)
+        self.cache.values_set_value(self.store_key(), self.value_key(serialized_entity), serialized_entity)
+
+    def delete(self, transform):
+        serialized_entity = serialize_trade_transform(transform)
+        self.cache.values_delete_value(self.store_key(), self.value_key(serialized_entity))
+
+    def store_all(self, transformations):
         self.log.debug(f'storing trade transform:[{len(transformations)}]')
-        self.__store_all(transformations)
-
-    def __store_overwrite(self, trade_transform: TradeTransform):
-        all_trade_transform = self.retrieve()
-        if trade_transform not in all_trade_transform:
-            all_trade_transform.append(trade_transform)
-            self.store(all_trade_transform)
-        else:
-            all_trade_transform = list([et for et in all_trade_transform if et != trade_transform])
-            all_trade_transform.append(trade_transform)
-            self.store(all_trade_transform)
-
-    def __store_all(self, trade_transformations):
-        key = self.options[TRADE_TRANSFORMATIONS_KEY]
-        entities_to_store = list([serialize_trade_transform(trade_transform) for trade_transform in trade_transformations])
-        self.cache.store(key, entities_to_store)
+        serialized_entities = list([serialize_trade_transform(tt) for tt in transformations])
+        self.cache.values_store(self.store_key(), serialized_entities, custom_key=self.value_key)
 
     def retrieve(self) -> List[TradeTransform]:
-        key = self.options[TRADE_TRANSFORMATIONS_KEY]
-        raw_entities = self.cache.fetch(key, as_type=list)
-        entities = list([deserialize_trade_transform(raw) for raw in raw_entities])
+        serialized_entities = self.cache.values_fetch(self.store_key())
+        entities = list([deserialize_trade_transform(se) for se in serialized_entities])
         self.log.debug(f'retrieving trade transforms:[{len(entities)}]')
         return entities
-
-    def remove(self, trade_transform):
-        trade_transformations = self.retrieve()
-        self.log.debug(f'removing trade transform [{trade_transform}]')
-        trade_transformations_without_deleted = list([tt for tt in trade_transformations if tt != trade_transform])
-        self.store(trade_transformations_without_deleted)
